4c3c40545da19e30b4518e7b138e06555fca996e
antonyle-dev <antony.quang@helloclever.co>
2025-12-02 23:41:35 +0700

    fix: update

---
+++ app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/trungql", (req, res) => {
+    res.send("Hello Trunql");
+});
+
+app.listen(PORT, () => {
+    console.log("🚀 ~ APP - ROOT is running on PORT:", PORT);
+});
